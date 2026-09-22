d78ab7db7388d45e83a4b63eae31fd2f7e63e26a
Balázs Bence <ankylo4444@gmail.com>
2026-09-22 09:53:34 +0200

    Itt a változtatás

--- Test.js
+++ Test.js
@@ -1,12 +1,24 @@
-class Test {
+class Test
+{
     static #_index = 0;
 	static get getIndex() {return Test.#_index}
     index = null;
     constructor() {
         this.index = Test.#_index++;
     }
+    a = 12
+    b = 10
     
     Kiir123() {
         console.log('123');
     }
+
+    Osszeadom() {
+        console.log(`Ez egy összeadás ${a + b}`)
+    }
+    
+
+    
+
+
 }
